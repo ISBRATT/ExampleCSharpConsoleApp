df5ce0dbc6d2a78c48d843542f82b2bbd6aa7b1f
Ian Bratt <BR14TTY@outlook.com>
2025-05-31 14:28:12 +0100

    Add project files.

---
+++ ExampleC#ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What is your name? ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}");
+
+Console.WriteLine("Please give me a number:");
+var number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please give me another number:");
+var number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("We will now add these numbers");
+var addition = number1 + number2;
+Console.WriteLine(addition);
