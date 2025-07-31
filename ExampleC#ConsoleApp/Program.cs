863f1d3faa03c17a5daa79b2a8efc25953ca4079
ISBRATT <br14tty@outlook.com>
2025-07-31 11:44:25 +0100

    Latest changes
    Update user prompts and change operation to multiplication
    
    Modified the program to ask for the user's first name and surname separately, enhancing the greeting to include both. Changed the arithmetic operation from addition to multiplication, with updated prompts and calculations to reflect this new functionality.

--- ExampleC#ConsoleApp/Program.cs
+++ ExampleC#ConsoleApp/Program.cs
@@ -1,13 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("What is your name? ");
-var name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}");
+using System;
 
+Console.WriteLine("What is your First name? ");
+var name = Console.ReadLine();
+Console.WriteLine("What is your Surname? ");
+var surname = Console.ReadLine();   
+Console.WriteLine($"Hello, {name} {surname}");
+
+Console.WriteLine("We are now going to multply two numbers:");
 Console.WriteLine("Please give me a number:");
+
 var number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Please give me another number:");
 var number2 = int.Parse(Console.ReadLine());
-Console.WriteLine("We will now add these numbers");
-var addition = number1 + number2;
+Console.WriteLine("We will now multiply these numbers");
+var addition = number1 * number2;
 Console.WriteLine(addition);
